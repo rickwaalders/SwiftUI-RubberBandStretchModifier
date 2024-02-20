5a959c4b52d584fa7fd46a165f937ee19edb4373
Snaggle <rwaalders@gmail.com>
2024-02-20 09:34:21 +0100

    Initial Commit

---
+++ SwiftUI-RubberBandStretchModifier/SwiftUI_RubberBandStretchModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_RubberBandStretchModifierApp.swift
+//  SwiftUI-RubberBandStretchModifier
+//
+//  Created by Rick Waalders on 20/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_RubberBandStretchModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
